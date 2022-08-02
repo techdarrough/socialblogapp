7c27867d032006f5d4466ec1187e08c572b539b5
Preston Darrough <techdarrough@gmail.com>
2022-08-02 15:38:16 -0700

    heart button components and firebase.rules added

---
+++ components/HeartButton.js
@@ -0,0 +1,38 @@
+import { firestore, auth, increment } from '../lib/firebase';
+import { useDocument } from 'react-firebase-hooks/firestore';
+
+// Allows user to heart or like a post
+export default function Heart({ postRef }) {
+    // Listen to heart document for currently logged in user
+    const heartRef = postRef.collection('hearts').doc(auth.currentUser.uid);
+    const [heartDoc] = useDocument(heartRef);
+
+    // async function to create to docs 
+    const addHeart = async () => {
+        const uid = auth.currentUser.uid;
+        const batch = firestore.batch();
+
+        batch.update(postRef, { heartCount: increment(1) });
+        //add likes
+        batch.set(heartRef, { uid });
+        // attach heart doc to user id
+        await batch.commit();
+        // commits data to server
+    };
+
+    // Removes heart doc by users referenced 
+    const removeHeart = async () => {
+        const batch = firestore.batch();
+
+        batch.update(postRef, { heartCount: increment(-1) });
+        batch.delete(heartRef);
+
+        await batch.commit();
+    };
+
+    return heartDoc?.exists ? (
+        <button onClick={removeHeart}>💔 Unheart</button>
+    ) : (
+        <button onClick={addHeart}>💗 Heart</button>
+    );
+}
--- pages/[username]/[slug].js
+++ pages/[username]/[slug].js
@@ -1,58 +1,9 @@
 import styles from '@styles/Post.module.css'
 import PostContent from '@components/PostContent';
 import { firestore, getUserWithUsername, postToJSON } from '@lib/firebase';
-
-
-
-// export async function getStaticProps({ params }) {
-//   const { username, slug } = params;
-//   const userDoc = await getUserWithUsername(username);
-
-//   let post;
-//   let path;
-
-//   if (userDoc) {
-//     const postRef = userDoc.ref.collection('posts').doc(slug);
-//     post = postToJSON(await postRef.get());
-
-//     path = postRef.path;
-//   }
-
-//   return {
-//     props: { post, path },
-//     revalidate: 5000,
-//   };
-// }
-
-// export async function getStaticPaths() {
-//   // Improve my using Admin SDK to select empty docs
-//   const snapshot = await firestore.collectionGroup('posts').get();
-
-//   const paths = snapshot.docs.map((doc) => {
-//     const { slug, username } = doc.data();
-//     return {
-//       params: { username, slug },
-//     };
-//   });
-
-//   return {
-//     // must be in this format:
-//     // paths: [
-//     //   { params: { username, slug }}
-//     // ],
-//     paths,
-//     fallback: 'blocking', // solves nexts no Server Side Page page rendered 
-//   };
-// }
-
-// export default function Post(props) {
-// const postRef = firestore.doc(props.)
-//   return (
-//     <main className={styles.container}>
-
-//     </main>
-//   );
-// }
+import HeartButton from '../../components/HeartButton';
+import AuthCheck from '../../components/AuthCheck';
+import Link from 'next/link';
 
 
 import { useDocumentData } from 'react-firebase-hooks/firestore';
@@ -74,6 +25,22 @@ export default function Post(props) {
         <p>
           <strong>{post.heartCount || 0} �</strong>
         </p>
+
+        <AuthCheck
+          fallback={
+            <Link href="/enter">
+              <button>� Sign Up</button>
+            </Link>
+          }
+        >
+          <HeartButton postRef={postRef} />
+        </AuthCheck>
+
+        {currentUser?.uid === post.uid && (
+          <Link href={`/admin/${post.slug}`}>
+            <button className="btn-blue">Edit Post</button>
+          </Link>
+        )}
 
       </aside>
     </main>
