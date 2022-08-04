df6a183b73bb0cb9a2901201330e32276aacb565
Preston Darrough <techdarrough@gmail.com>
2022-08-04 08:55:33 -0700

    Error Control added to user.hs

--- pages/[username]/index.js
+++ pages/[username]/index.js
@@ -4,51 +4,64 @@ import { getUserWithUsername, postToJSON } from '@lib/firebase';
 import Metatags from '@components/Metatags';
 
 // used to tell next to wait until data is fetch before render
-export async function getServerSideProps( { query }) {
+export async function getServerSideProps({ query }) {
     const { username } = query;
     const userDoc = await getUserWithUsername(username);
-    
-    
+
+
     // if not found 404 add try catch here later
-    if (!userDoc) {
+   try{ if (!userDoc) {
         return {
             notFound: true
         }
-    };
-    
+    };} catch(error) {
+        console.log(error)
+    }
+
     // Json serializable
     let user = null;
     let posts = null;
 
     // if user doc set user to userdoc data 
     //frame query to user.ref limit to 5 posts
-    if (userDoc) {
-        user = userDoc.data()
-        const postQuery = userDoc.ref
-            .collection('posts')
-            .where('published', '==', true)
-            .orderBy('createAt','desc')
-            .limit(5);
-        // convert to json data
+    try {
+        if (userDoc) {
+            user = userDoc.data()
+            const postQuery = userDoc.ref
+                .collection('posts')
+                .where('published', '==', true)
+                .orderBy('createAt', 'desc')
+                .limit(5);
+            // convert to json data
             posts = (await postQuery.get()).docs.map(postToJSON);
-    
-
-    };
 
 
-    return {
-        props:{ user, posts }, // passing to page as props
+        };
+
+
+        return {
+            props: { user, posts }, // passing to page as props
+        }
+
+    } catch (error) {
+        console.log(error)
     }
+
+
+
 }
 
-export default function UserProfilePage({user, posts}) {
+export default function UserProfilePage({ user, posts }) {
     console.log(user, posts)
+    try{
     return (
         <main>
             <Metatags title={user.username} description={`${user.username}'s public profile`} />
-            <UserProfile user={user}/>
-            <PostFeed posts={posts}/>
+            <UserProfile user={user} />
+            <PostFeed posts={posts} />
 
         </main>
-    )
+    )} catch(error) {
+        console.log(error)
+    }
 }
