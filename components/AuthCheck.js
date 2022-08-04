ebbe8ae6492af9aea30dd0e6e732c6c35b60ec64
Preston Darrough <techdarrough@gmail.com>
2022-08-04 09:22:33 -0700

    error control added to auth check

--- components/AuthCheck.js
+++ components/AuthCheck.js
@@ -6,6 +6,9 @@ import { UserContext } from "@lib/context";
 //component children only shown to logged in usr 
 
 export default function AuthCheck(props) {
+    try{
     const {username} = useContext(UserContext)
     return username ? props.children : props.fallback || <Link href="/enter">You must login first</Link>
-}
+    }catch(error){console.log(error)}}
+
+    
