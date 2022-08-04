36f604d7b912e22e6a105af1c96615787e9582cd
Preston Darrough <techdarrough@gmail.com>
2022-08-03 22:00:20 -0700

    ship it

--- components/Metatags.js
+++ components/Metatags.js
@@ -1,22 +1,20 @@
 import Head from 'next/head';
 
 export default function Metatags({
-  title = 'The Full Next.js + Firebase Course',
-  description = 'A complete Next.js + Firebase course by Fireship.io',
-  image = 'https://fireship.io/courses/react-next-firebase/img/featured.png',
+  title = 'The Blog',
+  description = 'Dev.to mock up',
 }) {
   return (
     <Head>
       <title>{title}</title>
       <meta name="twitter:card" content="summary" />
-      <meta name="twitter:site" content="@fireship_dev" />
+      <meta name="twitter:site" content="example meta" />
       <meta name="twitter:title" content={title} />
       <meta name="twitter:description" content={description} />
-      <meta name="twitter:image" content={image} />
+      
 {/* Open Graph meta */}
       <meta property="og:title" content={title} />
       <meta property="og:description" content={description} />
-      <meta property="og:image" content={image} />
     </Head>
   );
 }
